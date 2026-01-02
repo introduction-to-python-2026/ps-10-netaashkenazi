917e966886b61da83eb3ebd8597cb7dad6d3b436
netaashkenazi <neta.ashkenazi.email@gmail.com>
2026-01-02 15:05:57 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,23 +1,30 @@
-from image_utils import load_image, edge_detection
 from PIL import Image
-from skimage.filters import median
-from skimage.morphology import ball
-
-
-image_path = r"gallery_Party_Party_113.jpg"
-image1 = load_image(image_path)
-
-from skimage.filters import median
-from skimage.morphology import ball
-
-clean_image = median(image1, ball(3))
-
-
-
-image2 = edge_detection(clean_image)
-image2_copy = image2.copy()
-from PIL import Image
+import numpy as np
+from scipy.signal import convolve2d
 from matplotlib import pyplot as plt
+from PIL import Image
+import numpy as np
+from scipy.signal import convolve2d
+
+def load_image(path):
+    image = Image.open(path)
+    image_array = np.array(image)
+    return image_array
+
+def edge_detection(image):
+    image = np.mean(image, axis=2)
+    plt.imshow(image, cmap='gray')
+    kernelY = np.array([[1,2,1],
+                     [0,  0,  0],
+                     [-1,-2,-1]])
+    
+    kernelX = np.array([[-1, 0, 1],
+                     [-2,0,2],
+                        [-1,0,1]])
+    edgeX = convolve2d(image, kernelX, mode='same')
+    edgeY = convolve2d(image, kernelY, mode='same')
+    edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
+    return edgeMAG
 
 
 binary_image = np.where(image2_copy<60 ,0 ,1)
