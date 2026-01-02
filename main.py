77850b93d6693ce84947874946ee3dc6742a371b
netaashkenazi <neta.ashkenazi.email@gmail.com>
2026-01-02 14:44:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ from skimage.morphology import ball
 from matplotlib import pyplot as plt
 
 
-
 image_path = r"/content/gallery_Party_Party_113.jpg"
 image1 = load_image(image_path)
-image2 = edge_detection(image1)
+
+from skimage.filters import median
+from skimage.morphology import ball
+
+clean_image = median(image1, ball(3))
+
+
+
+image2 = edge_detection(clean_image)
 image2_copy = image2.copy()
 from PIL import Image
-
-binary_image = np.where(image2_copy<90 ,0 ,1)
+from matplotlib import pyplot as plt
+binary_image = np.where(image2_copy<60 ,0 ,1)
 edge_image = plt.imshow(binary_image, cmap='gray')
 plt.imsave('my_edges.png', binary_image, cmap='gray')
