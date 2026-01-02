3c24562025f013e63f552a92d61d6c4330492a9d
netaashkenazi <neta.ashkenazi.email@gmail.com>
2026-01-02 14:54:26 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,4 +1,4 @@
-image_path = r"/content/gallery_Party_Party_113.jpg"
+image_path = r"gallery_Party_Party_113.jpg"
 image1 = load_image(image_path)
 
 from skimage.filters import median
@@ -13,9 +13,6 @@ image2_copy = image2.copy()
 from PIL import Image
 from matplotlib import pyplot as plt
 
-plt.figure(figsize=(3,2))
-plt.hist(image2_copy.flatten(), bins =256);
-plt.show()
 
 binary_image = np.where(image2_copy<60 ,0 ,1)
 edge_image = plt.imshow(binary_image, cmap='gray')
