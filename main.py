091629e968d34bca8acea31fab51b25934a47e1e
netaashkenazi <neta.ashkenazi.email@gmail.com>
2026-01-05 17:20:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,32 +6,19 @@ from matplotlib import pyplot as plt
 
 
 image_path = r"gallery_Party_Party_113.jpg"
-
 image1 = load_image(image_path)
 
 
 
 from skimage.filters import median
-
 from skimage.morphology import ball
-
-
 
 clean_image = median(image1, ball(3))
 
 
-
-
-
-
-
 image2 = edge_detection(clean_image)
-
 image2_copy = image2.copy()
 
-from PIL import Image
-
-from matplotlib import pyplot as plt
 
 
 
